aaa922f1569413395b1adaaf13aabb029c2b111b
Serena <serenastephens@Serenas-MacBook-Air.local>
2019-02-28 13:44:07 -0700

    Created countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,42 @@
-$ (document).ready(function() {
+$(document).ready(function () {
     alert("Press 'OK' to Start The Game!!");
 })
 
-var countDown = "00:30";
+var number = 30;
+
+var intervalId;
+
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+    console.log(number);
+}
+
+function decrement() {
+    number--;
+    console.log(number);
 
 
-    $('.check').click(function() {
-        $('.check').not(this).prop('checked', false);
-    });
-    
-    
-              
+    $("#countDown").html("<p>Time Remaining: " + number + "</p>");
+
+    if (number == 0) {
+
+        stop();
+    }
+}
+
+function stop() {
+    clearInterval(intervalId);
+}
+
+run();
+
+
+
+
+
+$('.check').click(function () {
+    $('.check').not(this).prop('checked', false);
+});
+
+
