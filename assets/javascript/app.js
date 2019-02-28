c7503cdba2454a0aba99aae0051bdcd8b0825d82
Serena <serenastephens@Serenas-MacBook-Air.local>
2019-02-28 13:01:36 -0700

    Finished html layout and added functionality to page

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+$ (document).ready(function() {
+    alert("Press 'OK' to Start The Game!!");
+})
+
+var countDown = "00:30";
+
+
+    $('.check').click(function() {
+        $('.check').not(this).prop('checked', false);
+    });
+    
+    
+              
